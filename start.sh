f4ee088f16f4f88fdb2334eefb2d4efbccce642a
Ryz <mirco.heitmann@gmail.com>
2018-10-18 10:06:14 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,4 +5,3 @@ docker build -t dockj build
 docker run -d --name dockj -p 8080:8080 -p 50000:50000 -v "$PWD/config:/var/jenkins_home:rw" -v "$PWD/mvn:/root/.m2:rw" dockj
 # show status
 docker ps
-echo "run 'docker container inspect dockj' for more info, 'docker ps' and 'docker container stats dockj' are useful too"
